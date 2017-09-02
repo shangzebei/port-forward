51d962963d44fad338b991737269ab78a9e8070d
shangzebei <917058965@qq.com>
2017-09-02 18:12:18 +0800

    kkk

--- main.go
+++ main.go
@@ -3,12 +3,21 @@ package main
 import (
 	"port-info/port"
 	"time"
+	"fmt"
+	"github.com/shirou/gopsutil/mem"
 )
 
 func main() {
 	a := port.StartPortForward("0.0.0.0:8700", "192.168.0.88:13000")
 	a.AddStatics(&port.Limit{})
 	port.StartPortForward("0.0.0.0:8701", "192.168.0.88:13000")
+	v, _ := mem.VirtualMemory()
+
+	// almost every return value is a struct
+	fmt.Printf("Total: %v, Free:%v, UsedPercent:%f%%\n", v.Total, v.Free, v.UsedPercent)
+
+	// convert to JSON. String() is also implemented
+	fmt.Println(v)
 	for true {
 		time.Sleep(time.Second)
 		//fmt.Println("aaa= ", a.TotalByte.String())

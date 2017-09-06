773572dcaaf8f885dcb1705ac3cc3095c4d61665
shangzebei <917058965@qq.com>
2017-09-06 15:45:07 +0800

    kkk

--- port/limit_port.go
+++ port/limit_port.go
@@ -11,18 +11,19 @@ type Limit struct {
 	useByte    big.Int
 	speedBytes int
 }
+
 //50M  30M
 func (kk *Limit) AddHasByte(b int64) {
-  kk.maxBytes.Add(&kk.maxBytes,big.NewInt(int64(b)))
+	kk.maxBytes.Add(&kk.maxBytes, big.NewInt(int64(b)))
 }
-func (kk *Limit) ReSetSpeed()  {
+func (kk *Limit) ReSetSpeed() {
 
 }
-
 func (kk *Limit) StaticInfo(copyBytes int64, port *port) () {
 	fmt.Println(util.GetBytePeerSecond(float64(copyBytes)))
-	//kk.useByte.Add(&kk.useByte,big.NewInt(copyBytes))
-	//if kk.maxBytes.Sub(&kk.maxBytes,&kk.useByte).Int64()<= 0 {
-	//	port.StopForward()
-	//}
+	kk.useByte.Add(&kk.useByte, big.NewInt(copyBytes))
+	if kk.maxBytes.Int64() > 0 &&
+		kk.maxBytes.Sub(&kk.maxBytes, &kk.useByte).Int64() <= 0 {
+		port.StopForward()
+	}
 }

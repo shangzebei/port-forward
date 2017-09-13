415ed1def11b95383bf8e0fc837fe8dd0e395564
shangzebei <917058965@qq.com>
2017-09-13 17:30:28 +0800

    go to func

--- main.go
+++ main.go
@@ -3,9 +3,22 @@ package main
 import (
 	"runtime"
 	"port-forward/web"
+	"github.com/VividCortex/godaemon"
 )
 
 func main() {
+	//app := cli.NewApp()
+	//app.Name = "greet"
+	//app.Usage = "fight the loneliness!"
+	//app.Action = func(c *cli.Context) error {
+	//	fmt.Println("Hello friend!")
+	//	time.Sleep(time.Second*10)
+	//	return nil
+	//}
+	//app.Run(os.Args)
+
+	godaemon.MakeDaemon(&godaemon.DaemonAttr{})
 	runtime.GOMAXPROCS(runtime.NumCPU())
+
 	web.InitConfig().Run(":5555")
 }
